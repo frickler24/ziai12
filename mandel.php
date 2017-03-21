6c7f5773bad28b4204279c3332cf238e42f095f9
Lutz Eichler <github@frickler24.de>
2017-03-21 20:14:11 +0100

    vierfach parallel hat schon funktioniert

--- mandel.php
+++ mandel.php
@@ -3,11 +3,19 @@ header("Content-Type: image/png");
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+// Dimension of picture in pixel (always 4:3 ration for these mandelbrot pictures)
+$dim_x=1024;
+$dim_y=$dim_x / 4 * 3;
+
 $center_x = (isset($_GET["x"]))? $_GET["x"] : -0.5;
 $diameter_x = (isset($_GET["dx"]))? $_GET["dx"] : 1.5;
 $center_y = (isset($_GET["y"]))? $_GET["y"] : 0.0;
 $factor =  (isset($_GET["f"]))? $_GET["f"] : 1.0;
 $iter =  (isset($_GET["i"]))? $_GET["i"] : 100;
+$pic_min_x = (isset($_GET["pic_min_x"]))? $_GET["pic_min_x"] : 0;
+$pic_max_x = (isset($_GET["pic_max_x"]))? $_GET["pic_max_x"] : $dim_x;
+$pic_min_y = (isset($_GET["pic_min_y"]))? $_GET["pic_min_y"] : 0;
+$pic_max_y = (isset($_GET["pic_max_y"]))? $_GET["pic_max_y"] : $dim_y;
 if (isset($_GET["dy"])) $diameter_y = $_GET["dy"];	// else do nothing. is set later on.
 
 $min_x = $center_x - $diameter_x;
@@ -21,16 +29,13 @@ $iter = $iter * $factor;
 
 // echo "<p>min_x = $min_x, max_x = $max_x, min_y = $min_y, max_y = $max_y, dia_y = $diameter_y, iter = $iter, factor = $factor</p>";
 
-$dim_x=1024;
-$dim_y=768;
-
-$im = imageCreateTrueColor ($dim_x, $dim_y)
-  or die("Cannot Initialize new GD image stream");
+$im = imageCreateTrueColor ($pic_max_x - $pic_min_x, $pic_max_y - $pic_min_y)
+  or die("Cannot Initialize new GD image stream with $pic_max_x - $pic_min_x, $pic_max_y - $pic_min_y");
 $black_color = imagecolorallocate($im, 0, 0, 0);
 $white_color = imagecolorallocate($im, 255, 255, 255);
 
-for($y=0;$y<=$dim_y;$y++) {
-  for($x=0;$x<=$dim_x;$x++) {
+for ($y = $pic_min_y; $y <= $pic_max_y; $y++) {
+  for ($x = $pic_min_x; $x <= $pic_max_x; $x++) {
     $c1=$min_x+($max_x-$min_x)/$dim_x*$x;
     $c2=$min_y+($max_y-$min_y)/$dim_y*$y;
  
@@ -49,9 +54,12 @@ for($y=0;$y<=$dim_y;$y++) {
     if ($i < $iter) {
 	  $c = (3 * log ($i) / log ($iter - 1.0));
 	  // echo "$c\n";
-	  if ($c < 1) imagesetpixel ($im, $x, $dim_y - $y, imageColorAllocate ($im, (int)(255*$c), 0, 0));
-	  else if ($c < 2) imagesetpixel ($im, $x, $dim_y - $y, imageColorAllocate ($im, 255, (int)(255*$c-1.0), 0));
-	  else imagesetpixel ($im, $x, $dim_y - $y, imageColorAllocate ($im, 255, 255, (int)(255*$c-2.0)));
+	  if ($c < 1) imagesetpixel			($im, $x - $pic_min_x, $pic_max_y - $y,
+	  		imageColorAllocate ($im, (int)(255*$c), 0, 0));
+	  else if ($c < 2) imagesetpixel	($im, $x - $pic_min_x, $pic_max_y - $y,
+	  		imageColorAllocate ($im, 255, (int)(255*$c-1.0), 0));
+	  else imagesetpixel				($im, $x - $pic_min_x, $pic_max_y - $y,
+	  		imageColorAllocate ($im, 255, 255, (int)(255*$c-2.0)));
 	}
   }
 }
