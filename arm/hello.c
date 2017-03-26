bb70efd067e316cd346f6d98a9c500d395a55be7
Lutz Eichler <github@frickler24.de>
2017-03-26 11:12:51 +0200

    Versuch arm/hello.c

---
+++ arm/hello.c
@@ -0,0 +1,8 @@
+#include <armhf.h>
+#include <stdio.h>
+
+int main (int ac, char**av) {
+	printf ("Hello world!\n");
+	printf ("Congratulations, your docker environment seems to run on an arm architecture.\n");
+	return 0;
+}
