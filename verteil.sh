0fc206996168c4fa0e668a18e658e1526b09e982
Lutz Eichler <github@frickler24.de>
2017-10-08 10:57:56 +0200

    Cleanup unneeded files and add addnas

--- verteil.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set +ex
-
-# Copy all relevant parts to the given node and start building images
-
-scp $(ls -1t | head -10) pi@$1:ziai12/.
-ssh pi@$1 sh -c "pwd && cd ~/ziai12 && pwd && docker build -f Dockerfile.nginx -t mandel . && docker build -f Dockerfile.phpfpm -t fpmimage ."
-
--- zaehleApache.sh
+++
@@ -1,1 +0,0 @@
-watch -n 1 -d 'echo -n "Anzahl Apache2-Server-Prozesse: " ; ps alxww | grep apache2 | wc -l '
