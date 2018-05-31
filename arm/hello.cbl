80f7567fbbf3c2df73298feff624ab784d17db1a
Lutz Eichler <github@frickler24.de>
2018-05-31 18:08:17 +0200

    Aktualisiere Bilder

---
+++ arm/hello.cbl
@@ -0,0 +1,7 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. HALLO-WELT.
+PROCEDURE DIVISION.
+      DISPLAY "Hallo, du schnöde Welt!"
+	  DISPLAY "Hallo Welt!\n"
+	  DISPLAY "Herzlichen Glückwunsch: Deine Docker Infrastruktur läuft auf der arm Architektur!\n"
+STOP RUN.
